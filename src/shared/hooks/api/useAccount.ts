eba22126ee99f291fa2f5ecde7d26ed71e6adea1
iO <senpai9464@yandex.ru>
2025-07-15 19:52:44 +0300

    upd: delete mock

--- src/shared/hooks/api/useAccount.ts
+++ src/shared/hooks/api/useAccount.ts
@@ -19,9 +19,9 @@ type Account = Extend<
 >;
 
 const fetchAccount = async () => {
-  // const localAccountData = localStorage.getItem('user-logged-in');
-  const userAccount = validateResult(localAccountData, UserSchema);
-  console.log(userAccount);
+  const localAccountData = localStorage.getItem('user-logged-in');
+  const userAccount = validateResult(JSON.parse(localAccountData || '{}'), UserSchema);
+  // console.log(userAccount);
 
   const user = await proxy.get<AdditionalInformation>(`/api/web2/referral/referrer/${userAccount.user_id}`, {
     schema: AdditionalInformationSchema,
