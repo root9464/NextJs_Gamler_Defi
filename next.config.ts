df5c424eb624c99e3c37508e2719d8da389c5941
iO <senpai9464@yandex.ru>
2025-08-21 17:46:09 +0300

    upd: restructure layout components for better maintainability

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    optimizePackageImports: ['@ton/core', 'rxjs'],
+    optimizePackageImports: ['@ton/core'],
     gzipSize: true,
     webpackMemoryOptimizations: true,
   },
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import { PageFlow } from '@/components/layouts/page-flow';
 import { GlobalProvider } from '@/components/providers/global';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
-import { Toaster } from 'sonner';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -17,22 +15,7 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
   return (
     <html lang='en'>
       <body>
-        <GlobalProvider>
-          <PageFlow
-            classNames={{
-              content: 'flex max-h-[calc(100vh-64px)] w-full flex-col gap-8 py-4 px-[30px] desktop-sm:pl-6 desktop-sm:pr-[60px]',
-              // header: 'px-4 desktop-sm:pl-6 desktop-sm:pr-[60px]',
-            }}>
-            {children}
-            <Toaster
-              toastOptions={{
-                style: {
-                  width: 'fit-content',
-                },
-              }}
-            />
-          </PageFlow>
-        </GlobalProvider>
+        <GlobalProvider>{children}</GlobalProvider>
       </body>
     </html>
   );
