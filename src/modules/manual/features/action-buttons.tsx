8e41243aa7644efcee6378939ac42cb6ba20585b
SooweKun <apextoplega@mail.ru>
2025-08-22 20:10:26 +0300

    upd: minor

--- src/modules/manual/features/action-buttons.tsx
+++ src/modules/manual/features/action-buttons.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { useAtom } from 'jotai';
+import { useRouter } from 'next/navigation';
 import { progressAtom } from '../store/progress';
 
 export const ActionButtons = () => {
   const [currentStepState, setProgress] = useAtom(progressAtom);
+  const router = useRouter();
   return (
     <div className='flex flex-row gap-2'>
       {currentStepState.step > 0 && (
@@ -12,9 +14,13 @@ export const ActionButtons = () => {
           Назад
         </Button>
       )}
-      {currentStepState.step < 10 &&(
+      {currentStepState.step < 10 ? (
         <Button intent='primary' onClick={() => setProgress((prev) => ({ ...prev, step: prev.step + 1 }))}>
           Выполнил
+        </Button>
+      ) : (
+        <Button intent='outline' onClick={() => router.back()}>
+          Все готово
         </Button>
       )}
     </div>
