79dbd384ff6742bc3b0b78e2dc61530d52594f80
root <tempname00000001.cursor@gmail.com>
2025-09-15 10:08:30 +0300

    upd: minor

--- src/modules/video/scene/flow/socket-interface.tsx
+++ src/modules/video/scene/flow/socket-interface.tsx
@@ -97,6 +97,7 @@ export const SocketInterface: FC<SocketInterfaceProps> = ({ sessionId, children 
 
   const initSocket = useCallback(
     (userId: string) => {
+      console.log("initialization socket connection")
       const url = `wss://serv.gamler.online/web3/api/session/ws/sales_courage/${encodeURIComponent(sessionId)}/${encodeURIComponent(userId)}`;
       const socket = new SocketManager(url);
       socketRef.current = socket;
